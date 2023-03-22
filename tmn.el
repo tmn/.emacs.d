d013dea4dbbdc448bf4ab2f84331eb48adbe22bd
Tri Nguyen <mail@trimn.net>
2023-03-22 22:23:33 +0100

    Fix faulty yasnippet config + new org setup and cleanup

--- tmn.el
+++ tmn.el
@@ -162,7 +162,6 @@
   :init
   ; (load-theme 'doom-vibrant t)
   (load-theme 'doom-opera-light t)
-  ; (load-theme 'tsdh-light t)
   :config
   (progn
     (setq doom-themes-enable-bold t
@@ -223,7 +222,7 @@
   :straight (corfu :type git :host github :repo "minad/corfu")
   :custom
   (corfu-cycle t)
-  ;; (corfu-auto t)
+  (corfu-auto t)
   :config
   (global-corfu-mode))
 
@@ -661,30 +660,19 @@ parses its input."
   :config
   (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1))
 
-;; (use-package forge
-;;   :after magit)
-
+(use-package forge
+  :after magit)
 
 (use-package yasnippet
-  :init
-  (yas-global-mode 1)
+  :hook (prog-mode . yas-minor-mode)
+  :config
+  (yas-reload-all)
   (setq yas-snippet-dirs '("~/.emacs.d/snippets")))
 
 ;; Dart and flutter mode
 (use-package lsp-dart
   :after lsp-mode)
 
-;; run app from desktop without emulator
-;; (use-package hover)
-
-;;(use-package minimap
-;;  :commands (minimap-mode)
-;;  :bind (("C-c o  m" . minimap-mode))
-;;  :init
-;;  (setq minimap-window-location 'right
-;;        minimap-minimum-width 15
-;;        minimap-hide-fringes nil
-;;        minimap-width-fraction 0.13))
 
 ;; Package `java`
 (use-package lsp-java
@@ -861,81 +849,93 @@ parses its input."
   (diminish org-indent-mode))
 
 (use-package org
-  :straight (org-plus-contrib :type git :repo "https://git.sr.ht/~bzg/org-contrib")
+  :straight (:type built-in)
   :hook (org-mode . t/org-mode-setup)
-  :bind (:map org-mode-map
-              ;; Prevent Org from overriding the bindings for
-              ;; windmove. By default, these keys are mapped to
-              ;; `org-shiftleft', etc.
-              ("S-<left>" . nil)
-              ("S-<right>" . nil)
-              ("S-<up>" . nil)
-              ("S-<down>" . nil)
-
-              ;; Add replacements for the keybindings we just removed.
-              ;; C-<left> and C-<right> are unused by Org. C-<up> and
-              ;; C-<down> are bound to `org-backward-paragraph', etc.
-              ;; (but see below).
-              ("C-<left>" . #'org-shiftleft)
-              ("C-<right>" . #'org-shiftright)
-              ("C-<up>" . #'org-shiftup)
-              ("C-<down>" . #'org-shiftdown)
-
-              ;; By default, Org maps C-<up> to
-              ;; `org-backward-paragraph' instead of
-              ;; `backward-paragraph' (and analogously for C-<down>).
-              ;; However, it doesn't do the same remapping for the
-              ;; other bindings of `backward-paragraph' (e.g. M-{).
-              ;; Here we establish that remapping. (This is important
-              ;; since we remap C-<up> and C-<down> to other things,
-              ;; above. So otherwise there would be no easy way to
-              ;; invoke `org-backward-paragraph' and
-              ;; `org-forward-paragraph'.)
-              ([remap backward-paragraph] . #'org-backward-paragraph)
-              ([remap forward-paragraph] . #'org-forward-paragraph)
-
-              ;; See discussion of this function below.
-              ("C-M-RET" . #'radian-org-insert-heading-at-point)
-              ("C-M-<return>" . #'radian-org-insert-heading-at-point))
-  :bind* (;; Add the global keybindings for accessing Org Agenda and
-          ;; Org Capture that are recommended in the Org manual.
-          ("C-c a" . #'org-agenda)
-          ("C-c c" . #'org-capture))
-  ;; :bind (("C-c a" . org-agenda))
   :config
-  ;; If you try to insert a heading in the middle of an entry, don't
-  ;; split it in half, but instead insert the new heading after the
-  ;; end of the current entry.
-  (setq org-insert-heading-respect-content t)
+  (setq org-ellipsis " ▾"
+        org-hide-emphasis-markers t
+        org-src-fontify-natively t
+        org-fontify-quote-and-verse-blocks t
+        org-src-tab-acts-natively t
+        org-edit-src-content-indentation 2
+        org-hide-block-startup nil
+        org-src-preserve-indentation nil
+        org-startup-folded 'content
+        org-cycle-separator-lines 2)
 
-  ;; But add a new function for recovering the old behavior (see
-  ;; `:bind' above).
-  (defun radian-org-insert-heading-at-point ()
-    "Insert heading without respecting content.
-This runs `org-insert-heading' with
-`org-insert-heading-respect-content' bound to nil."
-    (interactive)
-    (let ((org-insert-heading-respect-content nil))
-      (org-insert-heading)))
+  (setq org-modules
+    '(org-crypt
+        org-habit
+        org-bookmark
+        org-eshell
+        org-irc))
 
-  ;; Show headlines but not content by default.
-  (setq org-startup-folded 'content)
+  (setq org-refile-targets '((nil :maxlevel . 1)
+                             (org-agenda-files :maxlevel . 1)))
 
-  ;; Make it possible to dim or hide blocked tasks in the agenda view.
-  (setq org-enforce-todo-dependencies t)
+  (setq org-outline-path-complete-in-steps nil)
+  (setq org-refile-use-outline-path t)
 
-  ;; Make C-a, C-e, and C-k smarter with regard to headline tags.
-  (setq org-special-ctrl-a/e t)
-  (setq org-special-ctrl-k t)
+  ;; (org-babel-do-load-languages
+  ;;  'org-babel-load-languages
+  ;;  '((emacs-lisp . t)
+  ;;    (ledger . t)))
 
-  (put 'org-tags-exclude-from-inheritance 'safe-local-variable
-       #'radian--list-of-strings-p)
 
-  ;; When you create a sparse tree and `org-indent-mode' is enabled,
-  ;; the highlighting destroys the invisibility added by
-  ;; `org-indent-mode'. Therefore, don't highlight when creating a
-  ;; sparse tree.
-  (setq org-highlight-sparse-tree-matches nil))
+  (use-package org-superstar
+    :after org
+    :hook (org-mode . org-superstar-mode)
+    :custom
+    (org-superstar-remove-leading-stars t)
+    (org-superstar-headline-bullets-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+
+
+  (set-face-attribute 'org-document-title nil :font "SF Mono" :weight 'bold :height 1.3)
+
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "SF Mono" :weight 'medium :height (cdr face)))
+
+  (require 'org-indent)
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+
+  ;; Get rid of the background on column views
+  (set-face-attribute 'org-column nil :background nil)
+  (set-face-attribute 'org-column-title nil :background nil)
+
+
+  ;; This is needed as of Org 9.2
+  (require 'org-tempo)
+
+  (add-to-list 'org-structure-template-alist '("sh" . "src sh"))
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist '("sc" . "src scheme"))
+  (add-to-list 'org-structure-template-alist '("ts" . "src typescript"))
+  (add-to-list 'org-structure-template-alist '("py" . "src python"))
+  (add-to-list 'org-structure-template-alist '("go" . "src go"))
+  (add-to-list 'org-structure-template-alist '("yaml" . "src yaml"))
+  (add-to-list 'org-structure-template-alist '("json" . "src json"))
+  )
+
+(use-package org-make-toc
+  :hook (org-mode . org-make-toc-mode))
 
 (use-package rst
   :mode (("\\.txt$" . rst-mode)
